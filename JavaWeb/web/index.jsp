e6a12f8c1d67de808445531e89a89e6c9bca59b1
kbq <2427059464@qq.com>
2019-05-01 00:49:30 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: admin
+  Date: 2019/4/30
+  Time: 14:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>卧槽</title>
+  </head>
+  <body>
+    卧槽你大爷！
+  </body>
+</html>
---
+++ src/cn/wannengde/java/HelloWorld.java
@@ -0,0 +1,22 @@
+package cn.wannengde.java;
+
+import java.util.ArrayList;
+
+//IDEA中代码模板所处的位置：settings-Editor-Live Templates/ Postfix Comletion
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("helloworld!");
+    //我觉得挺好
+        int i = 1;
+        ArrayList list = new ArrayList();
+        list.add(0,"a");
+
+        System.out.println("args = [" + args + "]");
+
+
+
+    }
+
+}
